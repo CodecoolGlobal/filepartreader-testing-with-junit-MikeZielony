bb636166cb8b43b3a7e83d536e5be8f114da9b49
MikeZielony <51535787+MikeZielony@users.noreply.github.com>
2020-07-13 08:29:30 +0200

    Add methods according implematations

--- src/main/java/FileWordAnalyzer.java
+++ src/main/java/FileWordAnalyzer.java
@@ -1,17 +1,55 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FileWordAnalyzer {
-    FilePartReader filePartReader = new FilePartReader();
+    FilePartReader filePartReader;
 
-    public List getWordsOrderedAlphabetically (){
-        return null;
+    public FileWordAnalyzer (FilePartReader filePartReader) {
+        this.filePartReader = filePartReader;
     }
 
-    public List getWordsContainingSubstring (String subString) {
-    return null;
+    private List<String> split() throws IOException {
+        String originalText = filePartReader.readLines().replaceAll("[.]*[,]*\\n*", "");
+        return Arrays.asList(originalText.split("\\W"));
     }
 
-    public List  getStringsWhichPalindromes () {
-        return null;
+    public List<String> getWordsOrderedAlphabetically() throws IOException {
+        List<String> splittedText = split();
+        splittedText.sort(String::compareToIgnoreCase);
+        return splittedText;
     }
+
+    public List<String> getWordsContainingSubstring(String subString) throws IOException {
+        List<String> wordsContainingSubstring = new ArrayList<>();
+        List<String> splittedText = split();
+
+        for (String word : splittedText) {
+            if (word.contains(subString)) {
+                wordsContainingSubstring.add(word);
+            }
+        }
+        return wordsContainingSubstring;
+    }
+
+    public List<String> getStringsWhichPalindromes() throws IOException {
+        List<String> palindromes = new ArrayList<>();
+        List<String> splittedText = split();
+
+        for (String word : splittedText) {
+            if (word.length() > 1 && word.equals(reverse(word))) {
+                palindromes.add(word);
+            }
+        }
+        return palindromes;
+    }
+
+    private String reverse(String word) {
+        StringBuilder input = new StringBuilder();
+        input.append(word);
+        input = input.reverse();
+        return input.toString();
+    }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        FilePartReader filePartReader = new FilePartReader();
+        FileWordAnalyzer fileWordAnalyzer = new FileWordAnalyzer(filePartReader);
+        filePartReader.setup("/Users/mzi/Desktop/zADANIA/filepartreader-testing-with-junit-MikeZielony/src/main/resources/text.txt", 1, 111);
+
+        System.out.println(fileWordAnalyzer.getStringsWhichPalindromes());
+        System.out.println(fileWordAnalyzer.getWordsOrderedAlphabetically());
+        System.out.println(fileWordAnalyzer.getWordsContainingSubstring("ja"));
+    }
 }
+
+`
