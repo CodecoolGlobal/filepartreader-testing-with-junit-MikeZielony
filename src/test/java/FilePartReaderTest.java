0d1b5a3aeebbb28311d1227eba53f635f75bcc25
MikeZielony <51535787+MikeZielony@users.noreply.github.com>
2020-08-17 20:51:46 +0200

    Add project from local

--- src/test/java/FilePartReaderTest.java
+++ src/test/java/FilePartReaderTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class FilePartReaderTest {
+
 }
--- src/test/java/FileWordAnalyzerTest.java
+++ src/test/java/FileWordAnalyzerTest.java
@@ -1,2 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FileWordAnalyzerTest {
+
+    FileWordAnalyzer fileWordAnalyzer = new FileWordAnalyzer();
+
+    @Test
+    public void testGetWordsOrderedAlphabetically() throws IOException {
+        List<String> expected = Arrays.asList("a", "from", "on");
+
+        assertEquals(expected, fileWordAnalyzer.getWordsOrderedAlphabetically());
+    }
+
+    @Test
+    public void testGetStringsWhichPalindromes() throws IOException {
+        List<String> expected = new ArrayList<>();
+        expected.add("kajak");
+        expected.add("rowor");
+        expected.add("dood");
+
+
+
+        assertEquals(expected, fileWordAnalyzer.getStringsWhichPalindromes());
+    }
 }
